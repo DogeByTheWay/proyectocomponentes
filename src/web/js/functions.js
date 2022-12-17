44e1448274b176636d435d609da64ca5735eafe4
Esteban Adarme <estebanadsa@gmail.com>
2022-12-17 16:40:35 +0100

    refactor(18):Pasar todo el html a js en primera fase

--- src/web/js/functions.js
+++ src/web/js/functions.js
@@ -8,6 +8,8 @@ function abrirProducto() {
 }
 
 window.onload = () => {
+    cargacesta();
+    cargaproducto();
     document.getElementById("c-barra").onclick = function () {
         let nav = document.getElementsByClassName("c-nav")[0];
         if(nav.style.display=="block"){
@@ -19,4 +21,228 @@ window.onload = () => {
     
     document.getElementById('cestaBtn').onclick = abrirCesta;
     document.getElementById('productoBtn').onclick = abrirProducto;
+}
+
+function cargacesta(){
+    document.getElementById("cesta").innerHTML+=`<div class="container mx-auto mt-10">
+    <div class="flex shadow-md my-10">
+      <div class="w-3/4 bg-white px-10 py-10">
+        <div class="flex justify-between border-b pb-8">
+          <h1 class="font-semibold text-2xl">Cesta de la compra</h1>
+          <h2 class="font-semibold text-2xl">3 Articulos</h2>
+        </div>
+        <div class="flex mt-10 mb-5">
+          <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Detalle Producto</h3>
+          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Cantidad</h3>
+          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Precio</h3>
+          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+        </div>
+        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+          <div class="flex w-2/5"> 
+            <div class="w-40">
+              <img class="h-24" src="./assets/1.jpg" alt="">
+            </div>
+            <div class="flex flex-col justify-between ml-4 flex-grow">
+              <span class="font-bold text-sm">Lenovo V15 Intel Core i5-1135G7/8GB/256GB SSD/15.6</span>
+              <span class="text-red-500 text-xs">Portatil</span>
+              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Eliminar</a>
+            </div>
+          </div>
+          <div class="flex justify-center w-1/5">
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+
+            <input class="mx-2 border text-center w-8" type="text" value="1">
+
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+          </div>
+          <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
+          <span class="text-center w-1/5 font-semibold text-sm">$400.00</span>
+        </div>
+
+        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+          <div class="flex w-2/5"> 
+            <div class="w-40">
+              <img class="h-24" src="./assets/2.jpg" alt="">
+            </div>
+            <div class="flex flex-col justify-between ml-4 flex-grow">
+              <span class="font-bold text-sm">PC Racing Ordenador Gaming AMD Ryzen 7 5700G/16GB/1TB SSD</span>
+              <span class="text-red-500 text-xs">PC</span>
+              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Eliminar</a>
+            </div>
+          </div>
+          <div class="flex justify-center w-1/5">
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+
+            <input class="mx-2 border text-center w-8" type="text" value="1">
+
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+          </div>
+          <span class="text-center w-1/5 font-semibold text-sm">$40.00</span>
+          <span class="text-center w-1/5 font-semibold text-sm">$40.00</span>
+        </div>
+
+        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+          <div class="flex w-2/5"> 
+            <div class="w-40">
+              <img class="h-24" src="./assets/3.jpg" alt="">
+            </div>
+            <div class="flex flex-col justify-between ml-4 flex-grow">
+              <span class="font-bold text-sm">Placa base Asus TUF GAMING B450-PLUS II</span>
+              <span class="text-red-500 text-xs">Placa Base</span>
+              <a href="#" class="font-semibold hover:text-red-500 text-gray-500 text-xs">Eliminar</a>
+            </div>
+          </div>
+          <div class="flex justify-center w-1/5">
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+            <input class="mx-2 border text-center w-8" type="text" value="1">
+
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+          </div>
+          <span class="text-center w-1/5 font-semibold text-sm">$150.00</span>
+          <span class="text-center w-1/5 font-semibold text-sm">$150.00</span>
+        </div>
+
+        <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
+      
+          <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
+          Continua llenando la Cesta
+        </a>
+      </div>
+
+      <div id="summary" class="w-1/4 px-8 py-10">
+        <h1 class="font-semibold text-2xl border-b pb-8">Resumen del pedido
+      </h1>
+        <div class="flex justify-between mt-10 mb-5">
+          <span class="font-semibold text-sm uppercase">3 Articulos</span>
+          <span class="font-semibold text-sm">590$</span>
+        </div>
+        <div>
+          <label class="font-medium inline-block mb-3 text-sm uppercase">Metodo de envio</label>
+          <select class="block p-2 text-gray-600 w-full text-sm">
+            <option>Envio Standard  - $10.00</option>
+          </select>
+          <p>Entrega Martes, 14 de Octubre</p>
+        </div>
+        <div class="py-10">
+          <label for="promo" class="font-semibold inline-block mb-3 text-sm uppercase">Codigo Promocion</label>
+          <input type="text" id="promo" placeholder="Introduce tu Codigo" class="p-2 text-sm w-full">
+        </div>
+        <button class="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Probar Codigo</button>
+        <div class="border-t mt-8">
+          <div class="flex font-semibold justify-between py-6 text-sm uppercase">
+            <span>Coste Total</span>
+            <span>$600</span>
+          </div>
+          <button class="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Ir a pago</button>
+        </div>
+      </div>
+
+    </div>
+  </div>`;
+}
+function cargaproducto(){
+    document.getElementById("producto").innerHTML+=`<div class="container px-5 py-24 mx-auto">
+    <div class="lg:w-4/5 mx-auto flex flex-wrap">
+      <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src="./assets/6.jpg">
+      <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+        <h2 class="text-sm title-font text-gray-500 tracking-widest">Tarjeta Grafica</h2>
+        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">ASUS GeForce RTX 2060 Dual OC EVO Edition 6GB GDDR6</h1>
+        <div class="flex mb-4">
+          <span class="flex items-center">
+            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+            </svg>
+            <span class="text-gray-600 ml-3">Impacto Medioambiental</span>
+          </span>
+          <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+            <a class="text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a class="ml-2 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a class="ml-2 text-gray-500">
+              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+              </svg>
+            </a>
+          </span>
+        </div>
+        <p class="leading-relaxed"></p>
+        <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+          <div class="flex">
+            <span class="mr-3"></span>
+          </div>
+          <div class="flex ml-6 items-center">
+            <span class="mr-3"></span>
+            <div class="relative">              
+              <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">                
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="flex">
+          <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
+          <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Al Carrito</button>
+        </div>
+      </div>
+      <div class="flex flex-col">
+        <div>
+          <h5 class="mt-8 text-lg font-bold">Descripcion del Producto</h5>
+          <hr>
+        </div>
+        <div class="mt-5">
+          <ul>
+            <li>Familia de procesadores de gráficos: NVIDIA</li>
+            <li>Procesador gráfico: GeForce RTX 2060</li>
+            <li>Máxima resolución: 7680 x 4320 Pixeles</li>
+            <li>CUDA: Si</li>
+            <li>Frecuencia del procesador: 1365 MHz</li>
+            <li>Soporte para proceso paralelo: No compatible</li>
+            <li>FireStream: No</li>
+            <li>Núcleos CUDA: 1920</li>
+            <li>Aumento de la velocidad de reloj del procesador: 1755 MHz</li>
+            <li>Frecuencia del procesador (modo OC): 1365 MHz</li>
+            <li>Velocidad de aceleración del reloj del procesador (modo OC): 1785 MHz</li>
+            <li>Capacidad memoria de adaptador gráfico: 6 GB</li>
+            <li>Tipo de memoria de adaptador gráfico: GDDR6</li>
+            <li>Ancho de datos: 192 bit</li>
+            <li>Velocidad de memoria del reloj: 14000 MHz</li>
+            <li> Puertos e Interfaces</li>
+            <li> Tipo de interfaz: PCI Express x16 3.0</li>
+            <li> Número de puertos HDMI: 2</li>
+            <li> Cantidad de puertos DVI-D: 1</li>
+            <li> Cantidad de DisplayPorts: 1</li>
+            <li> Versión HDMI: 2.0b</li>
+            <li>Versión de DisplayPort: 1.4</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>`;
 }
