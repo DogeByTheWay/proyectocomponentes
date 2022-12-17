bebe619da213c1ff0f0482b504eae5a783bd8d5b
Esteban Adarme <estebanadsa@gmail.com>
2022-12-17 16:30:47 +0100

    feat(7):Mega banner Home

--- src/web/tailwind/tailwind.config.js
+++ src/web/tailwind/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["../*.{html,js}"],
   theme: {
-    extend: {},
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+    }
   },
   plugins: [],
 }
