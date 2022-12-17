6ce5a48d8308bf39657733501e9e436e06737c0d
rioArriba <coscado@gmail.com>
2022-12-17 14:30:29 +0100

    fix(10): evento click en icono carrito para abrir modal. functions.js

--- src/web/js/functions.js
+++ src/web/js/functions.js
@@ -1,3 +1,12 @@
+function abrirCesta() {
+    let cesta= document.getElementById("cesta");
+    (cesta.open) ? cesta.close() : cesta.showModal();
+}
+function abrirProducto() {
+    let producto= document.getElementById("producto");
+    (producto.open) ? producto.close() : producto.showModal();
+}
+
 window.onload = () => {
     document.getElementById("c-barra").onclick = function () {
         let nav = document.getElementsByClassName("c-nav")[0];
@@ -7,4 +16,7 @@ window.onload = () => {
             nav.style = 'display:block;';
         } 
     }
+    
+    document.getElementById('cestaBtn').onclick = abrirCesta;
+    document.getElementById('productoBtn').onclick = abrirProducto;
 }
