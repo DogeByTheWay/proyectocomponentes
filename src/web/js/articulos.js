1343730547256a555e818d2ee330f6df6f0957c1
rioArriba <coscado@gmail.com>
2022-12-17 13:36:24 +0100

    feat(1.1): Datos estaticos en articulos.js generados

---
+++ src/web/js/articulos.js
@@ -0,0 +1,376 @@
+articulos=[
+    {
+    'id':'1',
+    "nombre" :"Lenovo V15 Intel Core i5-1135G7/8GB/256GB SSD/15.6",
+    'descripcion':`Procesador Intel Core i5-1135G7 (4C / 8T, 2.4 / 4.2GHz, 8MB)
+        Memoria RAM 8GB Soldered DDR4-3200
+        Almacenamiento 256GB SSD M.2 2242 PCIe 3.0x4 NVMe
+         Unidad óptica No dispone
+        Display 15.6" FHD (1920x1080) TN 250nits Anti-glare, 45% NTSC
+        Controlador gráfico Integrada Intel Iris Xe`,
+    'precio':429,
+    'categoriaMadre': 'ordenador',
+    'categoriaPropia' : 'portatil'
+},
+    {
+    'id':'2',
+    'nombre':'PC Racing Ordenador Gaming AMD Ryzen 7 5700G/16GB/1TB SSD ',
+    'descripcion':`Torre: Hummer MC Pro es la versión optimizada de la clásica Hummer MC, el chasis más espectacular de todo el catálogo de Nox. Con un diseño atractivo y elegante en color blanco mantiene sus prestaciones, entre ellas su impresionante lateral acrílico, desde el que asomarse a su impactante interior, incorporando la novedosa tecnología ARGB.
+        Fuente de alimentación: 550W 80 Plus Bronze
+        Procesador: AMD Ryzen 7 5700G 4.6GHz
+        Placa base: MSI / GIGABYTE / A520 / B450
+          Memoria RAM: 16GB DDR4 3200 MHz
+         Disco duro 1: 1TB SSD NVMe`,
+    'precio':869,
+    'categoriaMadre': 'ordenador',
+    'categoriaPropia' : 'pc'
+    },
+    {
+    'id':'3',
+    'nombre':'Placa base Asus TUF GAMING B450-PLUS II',
+    'descripcion':`CPU
+        AMD AM4 Socket for 3rd/2nd/1st Gen AMD Ryzen™/2nd and 1st Gen AMD Ryzen™ with Radeon™ Vega Graphics/Athlon™ with Radeon™ Vega Graphics Processors
+        * Refer to www.asus.com for CPU support list
+        Chipset
+        AMD B450
+        Memory
+        3rd/2nd/1st Gen AMD Ryzen™/ 2nd and 1st Gen AMD Ryzen™ with Radeon™ Vega Graphics/ Athlon™ with Radeon™ Vega Graphics Processors
+        4 x DIMM, Max. 128GB, DDR4 4400(O.C)/3533(O.C)/3466(O.C.)/3200(O.C.)/3000(O.C.)/2800(O.C.)/2666/2400/2133 MHz Un-buffered Memory
+        Dual Channel Memory Architecture
+        ECC Memory (ECC mode) support varies by CPU.
+        * Refer to www.asus.com for the Memory QVL (Qualified Vendors Lists).
+        * The maximum memory frequency supported varies by processor.`,
+    'precio':119,
+     'categoriaMadre': 'componente',
+     'categoriaPropia' : 'placa base'
+    },
+    {
+    'id':'4',
+    'nombre':'AMD Ryzen 7 5800X 3.8GHz', 
+    'descripcion':`# de núcleos de CPU 8
+         # de hilos 16
+      Reloj base 3.8GHz
+         Reloj de aumento máx. Hasta 4.7GHz
+        Caché L2 total 4MB
+         Caché L3 total 32MB
+        Desbloqueados Sí
+        CMOS TSMC 7nm FinFET
+        Package AM4
+        Versión de PCI Express PCIe 4.0
+         Solución térmica (PIB) No incluida
+        TDP/TDP predeterminado 105W
+        Memoria
+        Velocidad máxima de memoria Up to 3200MHz
+        Tipo de memoria DDR4
+         Funcionalidades principales
+        Tecnologías compatibles Tecnología AMD StoreMI
+        Utilidad AMD Ryzen™ Master AMD Ryzen™ VR-Ready Premium`,
+    'precio':161,
+    'categoriaMadre': 'componente',
+    'categoriaPropia' : 'procesador'
+    },
+    {
+    'id':'5',
+    'nombre':'Samsung 870 EVO SSD 2.5" 500GB SATA3 Negro',
+    'descripcion':`Unidad de estado sólido
+    Factor de forma de disco SSD: 2.5"
+    Interfaz: Serial ATA III
+    SDD, capacidad: 500 GB
+    Velocidad de lectura: 560 MB/s
+    Velocidad de escritura: 530 MB/s
+    Características
+    Color del producto: Negro
+    Algoritmos de seguridad soportados: 256-bit AES
+    Soporte TRIM: Si
+    Soporte S.M.A.R.T.: Si
+    Tiempo medio entre fallos: 1500000 h
+    Peso y dimensiones
+    Ancho: 100 mm
+    Profundidad: 70 mm
+    Altura: 7 mm
+    Peso: 86 g`,
+    'precio':43,
+    'categoriaMadre': 'componente',
+    'categoriaPropia' : 'disco duro'
+    },
+    {
+    'id':'6',
+    'nombre':'ASUS GeForce RTX 2060 Dual OC EVO Edition 6GB GDDR6', 
+    'descripcion':`
+    Procesador
+    Familia de procesadores de gráficos: NVIDIA
+    Procesador gráfico: GeForce RTX 2060
+    Máxima resolución: 7680 x 4320 Pixeles
+    CUDA: Si
+    Frecuencia del procesador: 1365 MHz
+    Soporte para proceso paralelo: No compatible
+    FireStream: No
+    Núcleos CUDA: 1920
+    Aumento de la velocidad de reloj del procesador: 1755 MHz
+    Frecuencia del procesador (modo OC): 1365 MHz
+    Velocidad de aceleración del reloj del procesador (modo OC): 1785 MHz
+    Memoria
+    Capacidad memoria de adaptador gráfico: 6 GB
+    Tipo de memoria de adaptador gráfico: GDDR6
+    Ancho de datos: 192 bit
+    Velocidad de memoria del reloj: 14000 MHz
+    Puertos e Interfaces
+    Tipo de interfaz: PCI Express x16 3.0
+    Número de puertos HDMI: 2
+    Cantidad de puertos DVI-D: 1
+    Cantidad de DisplayPorts: 1
+    Versión HDMI: 2.0b
+    Versión de DisplayPort: 1.4`,
+    'precio':300,
+    'categoriaMadre': 'componente',
+    'categoriaPropia' : 'tarjeta grafica'
+    },
+    {
+    'id':'7',
+    'nombre':'Team Group Delta White RGB DDR4 3200 PC4-25600 16GB 2x8GB CL16', 
+    'descripcion':`Peso y dimensiones
+    Ancho: 49 mm
+    Profundidad: 146,8 mm
+    Altura: 7 mm
+    Características
+    Memoria interna: 16 GB
+    Tipo de memoria interna: DDR4
+    Velocidad de memoria del reloj: 3200 MHz
+    Componente para: PC/servidor
+    Forma de factor de memoria: 288-pin DIMM
+    Diseño de memoria (módulos x tamaño): 2 x 8 GB
+    Tipo de memoria con búfer: Unregistered (unbuffered)
+    ECC: No
+    Latencia CAS: 16
+    Voltaje de memoria: 1.35 V
+    Ancho de banda de memoria (max): 19,2 GB/s
+    Tipo de enfriamiento: Disipador térmico
+    Retroiluminación: Si
+    Color de luz de fondo: Rojo/Verde/Azul
+    Sistema operativo Windows soportado: Si
+    Sistema operativo Linux soportado: Si`,
+    'precio':56,
+    'categoriaMadre': 'componente',
+    'categoriaPropia' : 'ram'
+    },
+    {
+    'id':'8',
+    'nombre':'Philips 27M1N3200VS/00 27" LED FullHD 165Hz', 
+    'descripcion':`Exhibición
+    Diagonal de la pantalla: 68,6 cm (27")
+    Brillo de la pantalla (típico): 250 cd / m²
+    Resolución de la pantalla: 1920 x 1080 Pixeles
+    Tiempo de respuesta: 4 ms
+    Tipo de retroiluminación: W-LED
+    Superficie de la pantalla: Mate
+    Relación de aspecto: 16:9
+    Razón de contraste (típica): 3000:1
+    Nombre comercial de la relación de contraste dinámico: Mega Infinity DCR
+    Máxima velocidad de actualización: 165 Hz
+    Ángulo de visión, horizontal: 178°
+    Ángulo de visión, vertical: 178°
+    Número de colores de la pantalla: 16,7 millones de colores
+    Tipo de pantalla: VA
+    Tiempo de respuesta (rápido): 1 ms
+    Tamaño de pixel: 0,3114 x 0,3114 mm
+    Densidad del pixel: 81,59 ppp
+    Intervalo de escaneado horizontal: 30 - 160 kHz
+    Intervalo de escaneado vertical: 48 - 165 Hz
+    Tamaño visible, horizontal: 59,8 cm
+    Tamaño visible, vertical: 33,6 cm
+    sRGB: Si
+    Dureza de la superficie: 3H
+    Cobertura sRGB (típica): 121,9%
+    Cobertura NTSC (típica): 101,5%
+    Cobertura Adobe RGB: 89,9%`,
+    'precio':117,
+    'categoriaMadre': 'audio-video',
+    'categoriaPropia' : 'monitor'
+    },
+    {
+    'id':'9',
+    'nombre':'Xiaomi Mi Smart Compact Proyector 120" FullHD 500 Lúmenes ANSI', 
+    'descripcion':`Proyector
+    Compatibilidad de tamaño de pantalla: 1016 - 5080 mm (40 - 200")
+    Brillo de proyector: 500 lúmenes ANSI
+    Tecnología de proyección: DLP
+    Resolución original del proyector: 1080p (1920x1080)
+    Sistema de lentes
+    Enfoque: Auto
+    Relación de alcance: 1.2
+    Vídeo
+    Señal analógica: NTSC
+    Alta Definición Total: Si
+    Puertos e Interfaces
+    Cantidad de puertos USB 2.0: 1
+    Entrada de audio (L,R): 1
+    Número de puertos HDMI: 1
+    Puerto DVI: No
+    Conexión
+    Ethernet: No
+    Wifi: Si
+    Bluetooth: Si
+    Versión de Bluetooth: 4.0 BLE`,
+    'precio':359,
+    'categoriaMadre': 'audio-video',
+    'categoriaPropia' : 'proyector'
+    },
+    {
+    'id':'10',
+    'nombre':'Xiaomi TV P1E 43" LED UltraHD 4K HDR10', 
+    'descripcion':`Exhibición
+    Diagonal de la pantalla: 109,2 cm (43")
+    Tipo HD: 4K Ultra HD
+    Tecnología de visualización: LED
+    Forma de la pantalla: Plana
+    Relación de aspecto nativa: 16:9
+    Formato de pantalla, ajustes: 4:3,16:9
+    Formato de vídeo soportado: 1080p,1440p,2160p
+    Formatos gráficos soportados: 1920 x 1080 (HD 1080),2560 x 1440,3840 x 2160
+    Brillo de pantalla: 270 cd/m²
+    Frecuencia nativa de refresco: 60 Hz
+    Ángulo de visión, horizontal: 178°
+    Ángulo de visión, vertical: 178°
+    Resolución de la pantalla: 3840 x 2160 Pixeles`,
+    'precio':199,
+    'categoriaMadre': 'audio-video',
+    'categoriaPropia' : 'tv'
+    },
+    {
+    'id':'11',
+    'nombre':'Canon Selphy CP1300 Impresora Portátil Color WiFi Negra', 
+    'descripcion':`Exhibición
+    Pantalla incorporada: Si
+    Pantalla: LCD
+    Diagonal de la pantalla: 8,13 cm (3.2")
+    Requisitos del sistema
+    Sistema operativo Windows soportado: Si
+    Sistema operativo MAC soportado: Si
+    Otros sistemas operativos soportados: Android,iOS
+    Puertos e Interfaces
+    Wifi: Si
+    Bluetooth: No
+    Ethernet: No
+    Puerto USB: Si
+    Impresión directa: Si
+    PictBridge: Si
+    Conector USB: Mini-USB B
+    Cantidad de puertos USB: 1
+    Peso y dimensiones
+    Ancho: 180,6 mm
+    Profundidad: 63,3 mm
+    Altura: 135,9 mm
+    Peso: 860 g`,
+    'precio':69,
+    'categoriaMadre': 'audio-video',
+    'categoriaPropia' : 'impresora'
+    },
+    {
+    'id':'12',
+    'nombre':'HyperX Quadcast S Micrófono RGB Condensador USB Autónomo', 
+    'descripcion':`Micrófono
+    Sensibilidad de micrófono: -36 dB
+    Micrófono, distorsión armónica total (THD): 0,05%
+    Micrófono, impedancia de salida: 32 Ohmios
+    Micrófono, relación señal/ruido: 90 dB
+    Tipo de dirección de micrófono: Omnidireccional
+    Puertos e Interfaces
+    Tecnología de conectividad: Alámbrico
+    Interfaz del dispositivo: USB
+    Puerto USB: Si
+    Conector USB: USB Tipo C
+    Desempeño
+    Color del producto: Negro
+    Retroiluminación LED: Si
+    Longitud de cable: 3 m
+    Control de energía
+    Fuente de energía: USB`,
+    'precio':98,
+    'categoriaMadre': 'audio-video',
+    'categoriaPropia' : 'microfono'
+    },
+    {
+    'id':'13',
+    'nombre':'Bose Companion 2 Series III Altavoces Multimedia Negro', 
+    'descripcion':`Puertos e Interfaces
+    Tecnología de conectividad: Alámbrico
+    Tipo de interfaz de altavoz: RCA
+    Auriculares: 3,5 mm
+    Peso y dimensiones
+    Peso: 1,1 kg
+    Ancho de altavoz: 8,6 cm
+    Profundidad de altavoces: 15,2 cm
+    Subida de altavoz: 19 cm
+    Control de energía
+    Alimentación: Corriente alterna
+    Voltaje de entrada AC: 100 - 240 V
+    Frecuencia de entrada AC: 50 - 60 Hz
+    Corriente de salida de adaptador AC: 1,2 A
+    Corriente: 0,6 A
+    Voltaje de entrada DC: 12 V
+    Altavoces
+    Uso recomendado: PC
+    Canales de salida de audio: 2.0 canales
+    Ubicación de altavoz: Mesa/estante
+    Caja de altavoz: Cerrado
+    Amplificador: Integrado
+    Altavoz: Si
+    Adecuado para uso en exteriores: No
+    Otras características
+    Salida de auriculares: Si`,
+    'precio':88,
+    'categoriaMadre': 'audio-video',
+    'categoriaPropia' : 'altavoces'
+    },
+    {
+    'id':'14',
+    'nombre':'Samsung MUF-256AB/EU 256GB USB 3.1', 
+    'descripcion':`Peso y dimensiones
+    Ancho: 23,6 mm
+    Profundidad: 18,8 mm
+    Altura: 7,3 mm
+    Peso: 3,1 g
+    Seguridad
+    Lector de huella digital: No
+    Contenido del embalaje
+    Cantidad por paquete: 1 pieza(s)
+    Diseño
+    Factor de forma: Girar
+    Color del producto: Negro, Acero inoxidable
+    Funciones de protección: Resistente a golpes, Resistente a la temperatura, Resistente al agua
+    Llavero: Si
+    Desempeño
+    Capacidad: 256 GB
+    Interfaz del dispositivo: USB tipo A
+    Versión USB: 3.1 (3.1 Gen 1)
+    Velocidad de lectura (USB 3.0 (3.1 Gen 1)): 200 MB/s
+    Conectar y usar (Plug and Play): Si`,
+    'precio':35,
+    'categoriaMadre': 'almacenamiento',
+    'categoriaPropia' : 'memoria-usb'
+    },
+    {
+    'id':'15',
+    'nombre':'Disco Duro Externo Toshiba Canvio Basics 2.5" 1TB USB 3.0', 
+    'descripcion':`Disco duro
+    Capacidad de disco duro: 1000 GB
+    Tipo de dispositivo: Unidad de disco duro
+    Puertos e Interfaces
+    Conexión USB: Si
+    Tecnología Thunderbolt (Rayo): No
+    Wifi: No
+    Versión USB: 3.0 (3.2 Gen 1)
+    Peso y dimensiones
+    Ancho: 119 mm
+    Profundidad: 70 mm
+    Altura: 20,5 mm
+    Peso: 149 g
+    Control de energía
+    USB con suministro de corriente: Si
+    Diseño
+    Color del producto: Negro`,
+    'precio':39,
+    'categoriaMadre': 'almacenamiento',
+    'categoriaPropia' : 'memoria-usb'
+    }    
+]
