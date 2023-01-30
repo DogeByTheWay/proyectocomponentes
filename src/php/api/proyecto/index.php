77016762e11ef537e5c291b43df0f712533db9cb
wendy aguilar <wendylizbethaguilarcoyago.com>
2023-01-30 12:04:43 +0100

    fix(#9)

--- src/php/api/proyecto/index.php
+++ src/php/api/proyecto/index.php
@@ -10,7 +10,7 @@ if($method == "OPTIONS") {
 }
 
 require_once __DIR__ . "/vendor/autoload.php";
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load(); 
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->load(); 
 $app = new App();
 $app->run();
--- src/php/api/proyecto/src/controllers/CategoriasController.php
+++ src/php/api/proyecto/src/controllers/CategoriasController.php
@@ -6,13 +6,6 @@ use App\response\HTTPResponse;
 use Exception;
  
 class CategoriasController {
- /*
-    private IActoresService $service;
- /*ANTES DEL FACTORY
-    function __construct() {
-        $this->service = new ActoresService();
-    }
-    */
  
     public function all(){
         
